96592ff5b9e2e241d576659797fc8a4da5da3fe1
Colin L. Rice <colin@daedrum.net>
2015-02-08 22:57:43 -0500

    Some more ramblings on interfaces

--- interfaces.go
+++ interfaces.go
@@ -1,13 +1,30 @@
 package node
 
+import (
+	"github.com/AutoRoute/l2"
+)
+
 // The layer two protocol takes a layer two device and returns the hash of the
 // Public Key of all neighbors it can find.
 type NeighborFinder interface {
-    Find(l2.FrameReadWriter) <-chan string
+	Find(l2.FrameReadWriter) <-chan string
+}
+
+type ReachabilityMap interface{}
+
+// A layer three connection allows nodes to communicate with each other, given
+// a hash and a connection. This connection sends Reachabilitymaps and proof of
+// delivery.
+type ControlConnection interface {
 }
 
 // The layer three control plane sends reachability information and other
 // control messages.
 type ControlPlane interface {
+	ReadabilityMaps() <-chan ReachabilityMap
+}
 
+// The layer three data plane sends packets.
+type DataPlane interface {
+	SendPacket([]byte)
 }
