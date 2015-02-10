f1f11b0c9148d4364cc89e2ee0971d59994a13d9
Colin L. Rice <colin@daedrum.net>
2015-02-10 01:35:12 -0500

    More fleshing out of interfaces

--- interfaces.go
+++ interfaces.go
@@ -10,19 +10,27 @@ type NeighborFinder interface {
 	Find(l2.FrameReadWriter) <-chan string
 }
 
-// A map of a fixed size representing an interfaces potential
-type ReachabilityMap interface{}
+// Dummy interface to represent public keys
+type PublicKey interface{}
 
-// A receipt listing packets which have been succesfully deliver.
-type PacketReceipt interface{}
+// A map of a fixed size representing an interfaces potential
+type ReachabilityMap interface {
+	IsReachable(p PublicKey)
+}
+
+// A receipt listing packets which have been succesfully delivered
+type PacketReceipt interface {
+	ListPackets() []string
+	Verify(p PublicKey) error
+}
 
 // Layer three interfaces for network control traffic
 type MapConnection interface {
-	SendMap(ReachabilityMap)
+	SendMap(ReachabilityMap) error
 	ReadabilityMaps() <-chan ReachabilityMap
 }
 type ReceiptConnection interface {
-	SendReceipt(PacketReceipt)
+	SendReceipt(PacketReceipt) error
 	PacketReceipts() <-chan PacketReceipt
 }
 
@@ -34,6 +42,6 @@ type ControlConnection interface {
 
 // The actual data connection. Should be done at the layer two level in order to be able to send congestion signals
 type DataConnection interface {
-	SendPacket([]byte)
+	SendPacket([]byte) error
 	Packets() <-chan []byte
 }
