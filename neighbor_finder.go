a096ec46d938c6f2aeaa0ce1bc825b23d7042f84
Eric Oswald <eoswald39@gmail.com>
2015-02-19 00:40:41 -0500

    Finished first version of neighbor finder functionality.

--- neighbor_finder.go
+++ neighbor_finder.go
@@ -1,6 +1,7 @@
 package node
 
 import (
+	"bytes"
   "github.com/AutoRoute/l2"
 )
 
@@ -8,10 +9,40 @@ import (
 // Public Key of all neighbors it can find.
 type NeighborFinder interface {
   Find(l2.FrameReadWriter) <-chan string
+	RespondToNewInstance(l2.FrameReadWriter) <-chan string
 }
 
-type l2 struct { }
+type layer2 struct { }
 
-func (l l2) Find(l2.FrameReadWriter frw) string {
-  
+func (nf layer2) Find(l2.FrameReadWriter frw) string {
+	c := make(chan string);
+	// Broadcast Hash
+	broadcastAddr := l2.MacToBytesOrDie("ff:ff:ff:ff:ff:ff");
+	localAddr := l2.MacToBytesOrDie("aa:bb:cc:dd:ee:00"); // TODO: pass own mac address
+	var p PublicKey; // TODO: pass public key
+	publicKeyHash := p.Hash();
+	initFrame := NewEthFrame(dest, src, 31337, publicKeyHash); //TODO: add real protocol
+	frw.WriteFrame(initFrame);
+	// Process Loop
+	go func () {
+		for {
+			newInstanceFrame := frw.ReadFrame();
+			src := newInstanceFrame.Source();
+			dest := newInstanceFrame.Destination();
+			if (bytes.Equal(src, localAddr)) {
+				continue; // Throw away if from me
+			}
+			if !(bytes.Equal(dest, localAddr) || bytes.Equal(dest, broadcastAddr)) {
+				continue; // Throw away if it wasn't to me or the broadcast address
+			}
+			c <- string (newInstanceFrame.Data());
+			if (bytes.Equal(dest, broadcastAddr)) { // Respond if to broadcast addr
+				var p PublicKey; // TODO: pass public key
+				publicKeyHash := p.Hash();
+				initFrame := NewEthFrame(src, localAddr, 31337, publicKeyHash); //TODO: add real protocol
+				frw.WriteFrame(initFrame);
+			}
+		}
+	}()
+	return c;
 }
