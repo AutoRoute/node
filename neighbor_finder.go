9a8f7e3d7f77f5e88ac9617802fc23e3e7a876b8
Colin L. Rice <colin@daedrum.net>
2015-02-28 23:36:45 -0500

    Fix neighbor finder given l2 refactoring changes

--- neighbor_finder.go
+++ neighbor_finder.go
@@ -17,10 +17,10 @@ type layer2 struct{}
 func (nf layer2) Find(frw l2.FrameReadWriter) <-chan string {
 	c := make(chan string)
 	// Broadcast Hash
-	broadcastAddr := l2.MacToBytesOrDie("ff:ff:ff:ff:ff:ff")
-	localAddr := l2.MacToBytesOrDie("aa:bb:cc:dd:ee:00") // TODO: pass own mac address
-	var protocol uint16 = 31337                          // TODO: add real protocol
-	var p PublicKey = pktest("test")                     // TODO: pass public key
+	broadcastAddr, _ := l2.MacToBytes("ff:ff:ff:ff:ff:ff")
+	localAddr, _ := l2.MacToBytes("aa:bb:cc:dd:ee:00") // TODO: pass own mac address
+	var protocol uint16 = 31337                        // TODO: add real protocol
+	var p PublicKey = pktest("test")                   // TODO: pass public key
 	publicKeyHash := []byte(p.Hash())
 	initFrame := l2.NewEthFrame(broadcastAddr, localAddr, protocol, publicKeyHash)
 	fmt.Println("Broadcasting packet.")
