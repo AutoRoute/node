4f44a8665793fa4ed06390b89f039d7e4a24bfee
Eric Oswald <eoswald39@gmail.com>
2015-03-02 06:20:16 -0500

    Just added Colin's testing code. Still need to rename and define things.

--- neighbor_finder_test.go
+++ neighbor_finder_test.go
@@ -5,9 +5,35 @@ import (
 	"testing"
 )
 
-func FindNeighbors(t *testing.T, mac string) {
-	dev := SocketDevice{}
-	nf := layer2{mac}
-	msg := <= Find(dev)
-	fmt.Println(msg)
+type testInterface struct {
+	in chan EthFrame
+	out chan EthFrame
+}
+
+func (t testInterface) ReadFrame() EthFrame {
+	return <- in
+}
+
+func (t testInterface) WriteFrame() (e EthFrame) {
+	out <- e
+}
+
+func CreatePairedInterface() (FrameReadWriter, FrameReadWriter) {
+	one := make(chan EthFrame)
+	two := make(chan EthFrame)
+	return testInterface{one,two}, testInterface{two, one}
+}
+
+func NewNeighborFinder(pk PublicKey) NeighborFinder {
+	return layertwo{pk}
+}
+
+func TestBasicExchange(t *testing.T) {
+	one, two := CreatePairedInterface()
+
+	nf1 := layertwo{public_key1}
+	nf2 := NewNeighborFinder(public_key2)
+
+	outone := nf1.Find(test_mac1, one)
+	outtwo := nf2.Find(test_mac2, two)
 }
