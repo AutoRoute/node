d3b01b512c0705d9eebf2dee4f3633838da9206c
Eric Oswald <eoswald39@gmail.com>
2015-03-03 17:48:26 -0500

    Moved some functions around and finished (hopefully) a working version of the test suite. Waiting for some PR issues to start testing.

--- neighbor_finder_test.go
+++ neighbor_finder_test.go
@@ -2,38 +2,41 @@ package node
 
 import (
 	"fmt"
+	"github.com/AutoRoute/l2"
 	"testing"
 )
 
 type testInterface struct {
-	in chan EthFrame
-	out chan EthFrame
+	in  chan l2.EthFrame
+	out chan l2.EthFrame
 }
 
-func (t testInterface) ReadFrame() EthFrame {
-	return <- in
+func (t testInterface) ReadFrame() l2.EthFrame {
+	return <-in
 }
 
-func (t testInterface) WriteFrame() (e EthFrame) {
+func (t testInterface) WriteFrame() (e l2.EthFrame) {
 	out <- e
 }
 
 func CreatePairedInterface() (FrameReadWriter, FrameReadWriter) {
-	one := make(chan EthFrame)
-	two := make(chan EthFrame)
-	return testInterface{one,two}, testInterface{two, one}
-}
-
-func NewNeighborFinder(pk PublicKey) NeighborFinder {
-	return layertwo{pk}
+	one := make(chan l2.EthFrame)
+	two := make(chan l2.EthFrame)
+	return testInterface{one, two}, testInterface{two, one}
 }
 
 func TestBasicExchange(t *testing.T) {
+	var test_mac1 string = "aa:bb:cc:dd:ee:00"
+	var test_mac2 string = "aa:bb:cc:dd:ee:11"
+
+	var public_key1 PublicKey = pktest("test1")
+	var public_key2 PublicKey = pktest("test2")
+
 	one, two := CreatePairedInterface()
 
-	nf1 := layertwo{public_key1}
+	nf1 := NewNeighborFinder(public_key1)
 	nf2 := NewNeighborFinder(public_key2)
 
-	outone := nf1.Find(test_mac1, one)
-	outtwo := nf2.Find(test_mac2, two)
+	outone = nf1.Find(test_mac1, one)
+	outtwo = nf2.Find(test_mac2, two)
 }
