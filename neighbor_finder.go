e5c923be918d462544c7bb1f8ccd9b511d060003
Eric Oswald <eoswald39@gmail.com>
2015-03-04 02:21:53 -0500

    Replaced macToBytesOrDie with MacToBytes. Installs now.

--- neighbor_finder.go
+++ neighbor_finder.go
@@ -19,13 +19,18 @@ type NeighborData struct {
 func NewNeighborData(pk PublicKey) NeighborData {
 	return NeighborData{pk}
 }
-
-func (n NeighborData) Find(mac string, frw l2.FrameReadWriter) <-chan string {
+func (n NeighborData) Find(mac string, frw l2.FrameReadWriter) (<-chan string, error) {
 	c := make(chan string)
 	// Broadcast Hash
-	broadcastAddr := l2.macToBytesOrDie("ff:ff:ff:ff:ff:ff")
-	localAddr := l2.macToBytesOrDie(mac) // TODO: decide on mac passing before merging
-	var protocol uint16 = 31337          // TODO: add real protocol
+	broadcastAddr, errb := l2.MacToBytes("ff:ff:ff:ff:ff:ff")
+	if errb != nil {
+		return c, errb
+	}
+	localAddr, errl := l2.MacToBytes(mac) // TODO: decide on mac passing before merging
+	if errl != nil {
+		return c, errl
+	}
+	var protocol uint16 = 31337 // TODO: add real protocol
 	publicKeyHash := []byte(n.pk.Hash())
 	initFrame := l2.NewEthFrame(broadcastAddr, localAddr, protocol, publicKeyHash)
 	fmt.Println("Broadcasting packet.")
@@ -66,5 +71,5 @@ func (n NeighborData) Find(mac string, frw l2.FrameReadWriter) <-chan string {
 			}
 		}
 	}()
-	return c
+	return c, nil // TODO: return channel error?
 }
