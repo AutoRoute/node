3f904aa58c4b02f8a295e17dd6ea8b2839392770
Eric Oswald <eoswald39@gmail.com>
2015-03-19 02:52:18 -0400

    Prints

--- neighbor_finder_net_test.go
+++ neighbor_finder_net_test.go
@@ -1,12 +1,14 @@
 package node
 
 import (
+	"fmt"
 	"github.com/AutoRoute/l2"
 	"sync"
 	"testing"
 )
 
 func TestNetExchange(t *testing.T) {
+	fmt.Println("Testing Basic Net Exchange")
 	var test_mac1 string = "aa:bb:cc:dd:ee:00"
 	var test_mac2 string = "aa:bb:cc:dd:ee:11"
 
@@ -40,4 +42,5 @@ func TestNetExchange(t *testing.T) {
 		go CheckReceivedMessage(outtwo, string(public_key1.Hash()), string(public_key2.Hash()), &wg)
 		wg.Wait()
 	}
+	fmt.Println("Done")
 }
