e260bd84d6f13f36c544f9e7ff0881afa5c041bd
Colin L. Rice <colin@daedrum.net>
2015-03-29 01:05:22 -0400

    Add tests for the crypto file

---
+++ crypto_test.go
@@ -0,0 +1,24 @@
+package node
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCrypto(t *testing.T) {
+	k, err := NewECDSAKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+	m := []byte("hello")
+	s := k.Sign(m)
+	if s.Key().Hash() != k.PublicKey().Hash() {
+		t.Fatalf("Expected %v != %v", s.Key().Hash(), k.PublicKey().Hash())
+	}
+	if s.Verify() != nil {
+		t.Fatal(s.Verify())
+	}
+	if !bytes.Equal(s.Signed(), m) {
+		t.Fatalf("Expected %v != %v", s.Signed(), m)
+	}
+}
