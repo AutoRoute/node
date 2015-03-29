1f94d8d626164cf645a421d11fdcc82f92d9f0df
Colin L. Rice <colin@daedrum.net>
2015-03-29 00:31:59 -0400

    Implement a whole lot of crypto wrappers

---
+++ crypto.go
@@ -0,0 +1,87 @@
+package node
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"errors"
+	"log"
+	"math/big"
+)
+
+type PrivateKey interface {
+	PublicKey() PublicKey
+	Sign([]byte) Signature
+}
+
+type PublicKey interface {
+	Hash() NodeAddress
+}
+
+type Signature interface {
+	Key() PublicKey
+	Verify() error
+	Signed() []byte
+}
+
+type privateECDSAEncoding struct {
+	k ecdsa.PrivateKey
+}
+
+func (p privateECDSAEncoding) PublicKey() PublicKey {
+	return ecdsaEncoding(p.k.PublicKey)
+}
+
+func (p privateECDSAEncoding) Sign(m []byte) Signature {
+	r, s, err := ecdsa.Sign(rand.Reader, &p.k, m)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return ecdsaSignature{r, s, ecdsaEncoding(p.k.PublicKey), m}
+}
+
+type ecdsaSignature struct {
+	r *big.Int
+	s *big.Int
+	k ecdsaEncoding
+	m []byte
+}
+
+func (e ecdsaSignature) Key() PublicKey {
+	return e.k
+}
+
+func (e ecdsaSignature) Verify() error {
+	k := ecdsa.PublicKey(e.k)
+	if !ecdsa.Verify(&k, e.m, e.r, e.s) {
+		return errors.New("Invalid Signature")
+	}
+	return nil
+}
+
+func (e ecdsaSignature) Signed() []byte {
+	return e.m
+}
+
+type ecdsaEncoding ecdsa.PublicKey
+
+func (e ecdsaEncoding) Hash() NodeAddress {
+	t1, err := e.X.MarshalText()
+	if err != nil {
+		panic(err)
+	}
+	t2, err := e.Y.MarshalText()
+	if err != nil {
+		panic(err)
+	}
+	return NodeAddress("ecdsa:P521:" + string(t1) + "," + string(t2))
+}
+
+func NewPublicKey() (PublicKey, error) {
+	private, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+	return ecdsaEncoding(private.PublicKey), nil
+}
--- merkle_tree.go
+++ merkle_tree.go
@@ -1,6 +1,10 @@
 package node
 
-import ()
+import (
+	"bytes"
+	"crypto/sha512"
+	"errors"
+)
 
 // A receipt listing packets which have been succesfully delivered
 type PacketReceipt interface {
@@ -9,10 +13,25 @@ type PacketReceipt interface {
 	Verify() error
 }
 
-type Signature interface {
-	Key() PublicKey
-	Verify() error
-	Signed() []byte
+func CreateMerkleReceipt(key PrivateKey, packets []PacketHash) merklereceipt {
+	old := make([]merklenode, 0)
+	for _, h := range packets {
+		old = append(old, merklenode{h, nil, nil})
+	}
+
+	for len(old) > 1 {
+		cur := make([]merklenode, 0)
+		for i, _ := range old {
+			if i%2 == 1 {
+				cur = append(cur, merklenode{"", &old[i], &old[i-1]})
+			}
+		}
+		if len(old)%2 == 1 {
+			cur = append(cur, merklenode{"", &old[len(old)-1], nil})
+		}
+		old = cur
+	}
+	return merklereceipt{old[0], key.Sign(old[0].Hash())}
 }
 
 type merklereceipt struct {
@@ -20,8 +39,46 @@ type merklereceipt struct {
 	signature Signature
 }
 
+func (m merklereceipt) Verify() error {
+	if !bytes.Equal(m.signature.Signed(), m.tree.Hash()) {
+		return errors.New("Signature does not match contents")
+	}
+	return m.Verify()
+}
+
+func (m merklereceipt) Source() NodeAddress {
+	return m.signature.Key().Hash()
+}
+
+func (m merklereceipt) ListPackets() []PacketHash {
+	return m.tree.ListLeafs()
+}
+
 type merklenode struct {
-	hash  []byte
+	hash  PacketHash
 	left  *merklenode
 	right *merklenode
+}
+
+func (m merklenode) ListLeafs() []PacketHash {
+	if len(m.hash) != 0 {
+		return []PacketHash{m.hash}
+	}
+	if m.right == nil {
+		return m.left.ListLeafs()
+	}
+	return append(m.left.ListLeafs(), m.right.ListLeafs()...)
+}
+
+func (m merklenode) Hash() []byte {
+	if len(m.hash) != 0 {
+		s := sha512.Sum512([]byte(m.hash))
+		return s[0:sha512.Size]
+	}
+	if m.right == nil {
+		s := sha512.Sum512(append(m.left.Hash()))
+		return s[0:sha512.Size]
+	}
+	s := sha512.Sum512(append(m.left.Hash(), m.right.Hash()...))
+	return s[0:sha512.Size]
 }
