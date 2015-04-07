a1c9b60ee72367d43558fa81d3de0e9a7ba8a8f1
Colin L. Rice <colin@daedrum.net>
2015-04-07 01:38:28 -0400

    Pull out routing into it's own structure and interface

--- router.go
+++ router.go
@@ -1,12 +1,9 @@
 package node
 
-import (
-	"log"
-)
-
 // A router handles all routing tasks that don't involve the local machine
 // including connection management, reachability handling, packet receipt
-// relaying, and (outstanding) payment tracking.
+// relaying, and (outstanding) payment tracking. AKA anything which doesn't
+// need the private key.
 type Router interface {
 	AddConnection(Connection)
 	DataConnection
@@ -17,25 +14,27 @@ type Router interface {
 
 type router struct {
 	pk PublicKey
-	// A chan down which we send packets destined for ourselves.
-	incoming chan Packet
 	// A map of public key hashes to connections
 	connections map[NodeAddress]Connection
 
 	reachability ReachabilityHandler
+	routing      RoutingHandler
 	receipt      ReceiptHandler
 	payment      PaymentHandler
 }
 
 func newRouter(pk PublicKey) Router {
-	r := newReceipt(pk.Hash())
-	return &router{
+	receipt := newReceipt(pk.Hash())
+	reach := newReachability(pk.Hash())
+	router := &router{
 		pk,
-		make(chan Packet),
 		make(map[NodeAddress]Connection),
-		newReachability(pk.Hash()),
-		newReceipt(pk.Hash()),
-		newPayment(pk.Hash(), r.PacketHashes())}
+		reach,
+		newRouting(pk, reach),
+		receipt,
+		newPayment(pk.Hash(), receipt.PacketHashes())}
+	go router.shovelSent()
+	return router
 }
 
 func (r *router) GetAddress() PublicKey {
@@ -50,6 +49,14 @@ func (r *router) SendPayment(p Payment) {
 	r.payment.SendPayment(p)
 }
 
+func (r *router) SendPacket(p Packet) error {
+	return r.routing.SendPacket(p)
+}
+
+func (r *router) Packets() <-chan Packet {
+	return r.routing.Packets()
+}
+
 func (r *router) AddConnection(c Connection) {
 	id := c.Key().Hash()
 	_, duplicate := r.connections[id]
@@ -60,42 +67,15 @@ func (r *router) AddConnection(c Connection) {
 	r.connections[id] = c
 
 	// Curry the id since the various sub connections don't know about it
+	r.routing.AddConnection(id, c)
 	r.reachability.AddConnection(id, c)
 	r.receipt.AddConnection(id, c)
 	r.payment.AddConnection(id, c)
-	go r.handleData(id, c)
 }
 
-func (r *router) handleData(id NodeAddress, p DataConnection) {
-	for packet := range p.Packets() {
-		err := r.sendPacket(packet, id)
-		if err != nil {
-			log.Printf("%q: Dropping packet destined to %q: %q", r.pk.Hash(), packet.Destination(), err)
-			continue
-		}
+func (r *router) shovelSent() {
+	for d := range r.routing.Routes() {
+		r.receipt.AddSentPacket(d.p, d.source, d.nexthop)
+		r.payment.AddSentPacket(d.p, d.source, d.nexthop)
 	}
-}
-
-func (r *router) SendPacket(p Packet) error {
-	return r.sendPacket(p, r.pk.Hash())
-}
-
-func (r *router) sendPacket(p Packet, src NodeAddress) error {
-	if p.Destination() == r.pk.Hash() {
-		log.Printf("%q: Routing packet to self", r.pk.Hash())
-		r.incoming <- p
-		return nil
-	}
-	next, err := r.reachability.FindNextHop(p.Destination())
-	if err != nil {
-		return err
-	}
-	log.Printf("%q: Routing packet to %q", r.pk.Hash(), next)
-	go r.receipt.AddSentPacket(p, src, next)
-	go r.payment.AddSentPacket(p, src, next)
-	return r.connections[next].SendPacket(p)
-}
-
-func (r *router) Packets() <-chan Packet {
-	return r.incoming
 }
