861621948ea591cf90f3d262856e2255d84b9282
Colin L. Rice <colin@daedrum.net>
2015-04-19 21:01:13 -0400

    Add the money interface, untested.

--- node.go
+++ node.go
@@ -8,21 +8,40 @@ import (
 // A Node is the highest level abstraction over the network. You receive packets
 // from it and send packets to it, and it takes care of everything else.
 type Node interface {
+	AddConnection(Connection)
 	DataConnection
 }
 
 type node struct {
+	Router
 	l              *sync.Mutex
-	r              Router
 	id             PrivateKey
 	outgoing       chan Packet
 	receipt_buffer []PacketHash
 	receipt_ticker <-chan time.Time
 	payment_ticker <-chan time.Time
+	m              Money
+}
+
+func NewNode(pk PrivateKey) Node {
+	n := &node{
+		newRouter(pk.PublicKey()),
+		&sync.Mutex{},
+		pk,
+		make(chan Packet),
+		nil,
+		time.Tick(time.Second),
+		time.Tick(time.Second),
+		fakeMoney{pk.PublicKey().Hash()},
+	}
+	go n.receivePackets()
+	go n.sendReceipts()
+	go n.sendPayments()
+	return n
 }
 
 func (n *node) receivePackets() {
-	for p := range n.r.Packets() {
+	for p := range n.Router.Packets() {
 		n.l.Lock()
 		n.receipt_buffer = append(n.receipt_buffer, p.Hash())
 		n.l.Unlock()
@@ -36,12 +55,18 @@ func (n *node) sendReceipts() {
 		r := CreateMerkleReceipt(n.id, n.receipt_buffer)
 		n.receipt_buffer = nil
 		n.l.Unlock()
-		n.r.SendReceipt(r)
+		n.Router.SendReceipt(r)
+	}
+}
+
+func (n *node) sendPayments() {
+	for range n.payment_ticker {
+		//TODO
 	}
 }
 
 func (n *node) SendPacket(p Packet) error {
-	return n.r.SendPacket(p)
+	return n.Router.SendPacket(p)
 }
 
 func (n *node) Packets() <-chan Packet {
