daabbffcd8ea0723b45bb39e06baf65cbd870f13
Dimitar Dimitrov <newbrict@gmail.com>
2015-04-21 21:43:48 -0400

    bloom filters now track distance, sizing needs to be addressed.

--- bloom_reachability_map.go
+++ bloom_reachability_map.go
@@ -6,49 +6,86 @@ import (
 )
 
 type BloomReachabilityMap struct {
-	f        *bloom.BloomFilter
-	distance uint8
+	filters      []*bloom.BloomFilter
+	conglomerate *bloom.BloomFilter
 }
 
 func NewBloomReachabilityMap() ReachabilityMap {
+	fs := make([]*bloom.BloomFilter, 1)
+	fs[0] = bloom.New(1000, 4)
+
 	m := BloomReachabilityMap{
-		f:        bloom.New(1000, 4),
-		distance: 0,
+		filters:      fs,
+		conglomerate: fs[0].Copy(),
 	}
 	return m
 }
 
 func (m BloomReachabilityMap) IsReachable(n NodeAddress) bool {
 	entry := []byte(n)
-	res := m.f.Test(entry)
+	res := m.conglomerate.Test(entry)
 	return res
 }
 
 func (m BloomReachabilityMap) AddEntry(n NodeAddress) {
 	entry := []byte(n)
-	m.f.Add(entry)
+	m.filters[0].Add(entry)
+	m.conglomerate.Add(entry)
 }
 
 func (m BloomReachabilityMap) Increment() {
-	m.distance++
+	newZeroth := make([]*bloom.BloomFilter, 1)
+	newZeroth[0] = bloom.New(1000, 4)
+	m.filters = append(newZeroth, m.filters...)
 }
 
 func (m BloomReachabilityMap) Merge(nr ReachabilityMap) error {
 	var err error
 
 	if n, ok := nr.(BloomReachabilityMap); ok {
-		err = m.f.Merge(n.f)
+		if len(m.filters) < len(n.filters) {
+			for k, v := range m.filters {
+				err = v.Merge(n.filters[k])
+				if err != nil {
+					return err
+				}
+			}
+			// append the remaining filters
+			m.filters = append(m.filters, n.filters[len(n.filters):]...)
+		} else {
+			for k, v := range n.filters {
+				err = m.filters[k].Merge(v)
+				if err != nil {
+					return err
+				}
+			}
+		}
 	} else {
 		err = fmt.Errorf("Mismatched reachability map types")
+		return err
 	}
-
+	// reconstruct the conglomerate
+	for _, v := range m.filters {
+		err = m.conglomerate.Merge(v)
+		if err != nil {
+			return err
+		}
+	}
 	return err
 }
 
 func (m BloomReachabilityMap) Copy() ReachabilityMap {
+	newFilters := make([]*bloom.BloomFilter, len(m.filters))
+
+	// copy each filter
+	for k, v := range m.filters {
+		newFilters[k] = v.Copy()
+	}
+	newConglomerate := m.conglomerate.Copy()
+
 	mc := BloomReachabilityMap{
-		f:        m.f.Copy(),
-		distance: m.distance,
+		filters:      newFilters,
+		conglomerate: newConglomerate,
 	}
 	return mc
 }
--- bloom_reachability_map_test.go
+++ bloom_reachability_map_test.go
@@ -6,21 +6,35 @@ import (
 
 func TestSBRM(t *testing.T) {
 	a := NodeAddress("1")
-	s := NewBloomReachabilityMap()
-	s.AddEntry(a)
 
-	if !(s.IsReachable(a)) {
-		t.Fatalf("expected %v to be reachable", s)
+	bloomMap1 := NewBloomReachabilityMap()
+	bloomMap1.AddEntry(a)
+
+	if !(bloomMap1.IsReachable(a)) {
+		t.Fatalf("expected %s to be reachable in %v", a, bloomMap1)
 	}
 
-	b := NewBloomReachabilityMap()
-
-	err := b.Merge(s)
-	if err != nil {
-		t.Fatal(err)
+	// Test Merge
+	bloomMap2 := NewBloomReachabilityMap()
+	err1 := bloomMap2.Merge(bloomMap1)
+	if err1 != nil {
+		t.Fatal(err1)
 	}
 
-	if !(b.IsReachable(a)) {
-		t.Fatalf("expected %v to be reachable", s)
+	if !(bloomMap2.IsReachable(a)) {
+		t.Fatalf("expected %s to be reachable in %v", a, bloomMap2)
 	}
+
+	// Test Merge after Increment
+	bloomMap3 := NewBloomReachabilityMap()
+	bloomMap3.Increment()
+	err2 := bloomMap3.Merge(bloomMap2)
+	if err2 != nil {
+		t.Fatal("A merge across different distance SBRM's failed")
+	}
+
+	if !(bloomMap3.IsReachable(a)) {
+		t.Fatalf("expected %s to be reachable in %v", a, bloomMap3)
+	}
+
 }
