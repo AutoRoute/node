52871b731f9af8cd10a996a3fd4b37fc6ba4cbdf
Eric Oswald <eoswald39@gmail.com>
2015-05-23 23:44:14 -0400

    Created skeleton for SSHConnection interface, fixed SSH key authentication by creating a signer, and fixed Close() to be consistent with SSHConnection being a singular session.

--- ssh.go
+++ ssh.go
@@ -1,6 +1,7 @@
 package node
 
 import (
+	"fmt"
 	"golang.org/x/crypto/ssh"
 )
 
@@ -10,44 +11,56 @@ type SSHConnection struct {
 }
 
 func (s SSHConnection) SendMap(ReachabilityMap) error {
+	return nil
 }
 
 func (s SSHConnection) ReachabilityMaps() <-chan ReachabilityMap {
+	return nil
 }
 
 func (s SSHConnection) SendReceipts() <-chan PacketReceipt {
+	return nil
 }
 
 func (s SSHConnection) PacketReceipts() <-chan PacketReceipt {
+	return nil
 }
 
 func (s SSHConnection) SendPayment(Payment) error {
+	return nil
 }
 
 func (s SSHConnection) Payments() <-chan Payment {
+	return nil
 }
 
-func (s SSHConnectoin) SendPacket(Packet) error {
+func (s SSHConnection) SendPacket(Packet) error {
+	return nil
 }
 
 func (s SSHConnection) Packets() <-chan Packet {
+	return nil
 }
 
 func (s SSHConnection) Key() PublicKey {
+	return nil
 }
 
 func (s SSHConnection) Close() error {
-	for i := 0; i < len(ssessions); i++ {
-		s.sessions[i].Close()
-	}
+	err := s.session.Close()
+	return err
 }
 
-func EstablishSSH(addresses []node.NodeAddress, key NodeAddress) []*SSHConnection {
+func EstablishSSH(addresses []NodeAddress, key PrivateKey) []*SSHConnection {
 	username := "node-username"
+	signer, err := ssh.NewSignerFromKey(key)
+	if err != nil {
+		panic("Failed to create signer from key")
+	}
 	config := &ssh.ClientConfig{
 		User: username,
 		Auth: []ssh.AuthMethod{
-			ssh.PublicKeys(key),
+			ssh.PublicKeys(signer),
 		},
 	}
 
