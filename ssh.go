411b5c977a0d06d57e9715cc558050ce143d870d
Colin L. Rice <colin@daedrum.net>
2015-07-04 04:02:25 -0400

    Make ssh.go pass the race detector

--- ssh.go
+++ ssh.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net"
+	"sync"
 
 	"golang.org/x/crypto/ssh"
 )
@@ -22,14 +23,25 @@ type SSHConnection struct {
 	reqs  <-chan *ssh.Request
 	c     map[string]*SSHChannel
 	e     map[string]*json.Encoder
+	el    map[string]*sync.Mutex
 	d     map[string]*json.Decoder
+	dl    map[string]*sync.Mutex
+	lock  *sync.Mutex
 }
 
 // Constructs a new SSHConnection given the various items returned by the /x/c/ssh library.
 // Does *not* call listen() or connect() on the SSHConnection, which is required
 // to establish the various required channels.
 func NewSSHConnection(conn ssh.Conn, chans <-chan ssh.NewChannel, reqs <-chan *ssh.Request) *SSHConnection {
-	s := &SSHConnection{conn, chans, reqs, make(map[string]*SSHChannel), make(map[string]*json.Encoder), make(map[string]*json.Decoder)}
+	s := &SSHConnection{conn,
+		chans,
+		reqs,
+		make(map[string]*SSHChannel),
+		make(map[string]*json.Encoder),
+		make(map[string]*sync.Mutex),
+		make(map[string]*json.Decoder),
+		make(map[string]*sync.Mutex),
+		&sync.Mutex{}}
 	// Set up various session types we want.
 	s.c["reachability"] = nil
 	s.c["receipt"] = nil
@@ -64,106 +76,164 @@ func (s *SSHConnection) connect() error {
 
 func (s *SSHConnection) listen() {
 	for nc := range s.chans {
+		s.lock.Lock()
 		if _, ok := s.c[nc.ChannelType()]; !ok {
 			nc.Reject(ssh.UnknownChannelType, "Unknown channel type")
+			s.lock.Unlock()
 			continue
 		}
 		if s.c[nc.ChannelType()] == nil {
 			c, r, err := nc.Accept()
 			if err != nil {
 				log.Printf("Error accepting channel request: %v", err)
+				s.lock.Unlock()
 				continue
 			}
 			s.c[nc.ChannelType()] = &SSHChannel{c, r}
+			s.el[nc.ChannelType()] = &sync.Mutex{}
+			s.el[nc.ChannelType()].Lock()
 			s.e[nc.ChannelType()] = json.NewEncoder(c)
+			s.el[nc.ChannelType()].Unlock()
+			s.dl[nc.ChannelType()] = &sync.Mutex{}
+			s.dl[nc.ChannelType()].Lock()
 			s.d[nc.ChannelType()] = json.NewDecoder(c)
+			s.dl[nc.ChannelType()].Unlock()
 		} else {
 			nc.Reject(ssh.ConnectionFailed, "Connection already established")
 		}
+		s.lock.Unlock()
 	}
 }
 
 func (s *SSHConnection) connectChan(name string) error {
+	s.lock.Lock()
+	defer s.lock.Unlock()
 	c, r, err := s.conn.OpenChannel(name, nil)
 	if err != nil {
 		return err
 	}
 	s.c[name] = &SSHChannel{c, r}
+	s.el[name] = &sync.Mutex{}
+	s.el[name].Lock()
 	s.e[name] = json.NewEncoder(c)
+	s.el[name].Unlock()
+	s.dl[name] = &sync.Mutex{}
+	s.dl[name].Lock()
 	s.d[name] = json.NewDecoder(c)
+	s.dl[name].Unlock()
 	return nil
 }
 
 func (s *SSHConnection) SendMap(m ReachabilityMap) error {
+	s.el["reachability"].Lock()
+	defer s.el["reachability"].Unlock()
 	return s.e["reachability"].Encode(m)
 }
 
 func (s *SSHConnection) ReachabilityMaps() <-chan ReachabilityMap {
 	c := make(chan ReachabilityMap)
 	go func() {
-		var v BloomReachabilityMap
-		err := s.d["reachability"].Decode(&v)
-		if err != nil {
-			log.Print(err)
-			close(c)
-		} else {
-			c <- v
+		s.lock.Lock()
+		l := s.dl["reachability"]
+		s.lock.Unlock()
+		for {
+			l.Lock()
+			var v BloomReachabilityMap
+			err := s.d["reachability"].Decode(&v)
+			l.Unlock()
+			if err != nil {
+				log.Print(err)
+				close(c)
+				return
+			} else {
+				c <- v
+			}
 		}
 	}()
 	return c
 }
 
 func (s *SSHConnection) SendReceipt(r PacketReceipt) error {
+	s.el["receipt"].Lock()
+	defer s.el["receipt"].Unlock()
 	return s.e["receipt"].Encode(r)
 }
 
 func (s *SSHConnection) PacketReceipts() <-chan PacketReceipt {
 	c := make(chan PacketReceipt)
 	go func() {
-		var v PacketReceipt
-		err := s.d["receipt"].Decode(&v)
-		if err != nil {
-			log.Print(err)
-			close(c)
-		} else {
-			c <- v
+		s.lock.Lock()
+		l := s.dl["receipt"]
+		s.lock.Unlock()
+		for {
+			l.Lock()
+			var v PacketReceipt
+			err := s.d["receipt"].Decode(&v)
+			l.Unlock()
+			if err != nil {
+				log.Print(err)
+				close(c)
+				return
+			} else {
+				c <- v
+			}
 		}
 	}()
 	return c
 }
 
 func (s *SSHConnection) SendPayment(p PaymentHash) error {
+	s.el["payment"].Lock()
+	defer s.el["payment"].Unlock()
 	return s.e["payment"].Encode(p)
 }
 
 func (s *SSHConnection) Payments() <-chan PaymentHash {
 	c := make(chan PaymentHash)
 	go func() {
-		var v PaymentHash
-		err := s.d["payment"].Decode(&v)
-		if err != nil {
-			log.Print(err)
-			close(c)
-		} else {
-			c <- v
+		s.lock.Lock()
+		l := s.dl["payment"]
+		s.lock.Unlock()
+		for {
+			l.Lock()
+			var v PaymentHash
+			err := s.d["payment"].Decode(&v)
+			l.Unlock()
+			if err != nil {
+				log.Print(err)
+				close(c)
+				return
+			} else {
+				c <- v
+			}
 		}
 	}()
 	return c
 }
 
 func (s *SSHConnection) SendPacket(p Packet) error {
+	s.el["packet"].Lock()
+	defer s.el["packet"].Unlock()
 	return s.e["packet"].Encode(p)
 }
 
 func (s *SSHConnection) Packets() <-chan Packet {
 	c := make(chan Packet)
 	go func() {
-		var v Packet
-		err := s.d["packet"].Decode(&v)
-		if err != nil {
-			close(c)
-		} else {
-			c <- v
+		s.lock.Lock()
+		l := s.dl["packet"]
+		s.lock.Unlock()
+		for {
+			l.Lock()
+			var v Packet
+			err := s.d["packet"].Decode(&v)
+			l.Unlock()
+			if err != nil {
+				close(c)
+				return
+			} else {
+				c <- v
+			}
 		}
 	}()
 	return c
