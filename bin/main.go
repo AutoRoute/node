0058a8ef0aa0d00c7ca34df6dedf41a9f9d7f24c
Eric Oswald <eoswald39@gmail.com>
2015-07-13 00:26:30 -0400

    Implemented finding neigbor link local addresses in binary.

--- bin/main.go
+++ bin/main.go
@@ -4,17 +4,37 @@ import (
 	"github.com/AutoRoute/l2"
 	"github.com/AutoRoute/node"
 
+	"fmt"
 	"log"
 	"net"
 )
 
-func FindNeighbors(dev net.Interface, key node.PublicKey) <-chan node.NodeAddress {
+func GetLinkLocalAddr(dev net.Interface) (*net.IPAddr, error) {
+	cidr_ll_addr, err := dev.Addrs()
+	if err != nil {
+		return nil, err
+	}
+
+	ll_addr, _, err := net.ParseCIDR(cidr_ll_addr[1].String())
+	if err != nil {
+		return nil, err
+	}
+
+	ll_addr_zone := fmt.Sprintf("%s%%%s", ll_addr.String(), dev.Name)
+	resolved_ll_addr, err := net.ResolveIPAddr("ip6", ll_addr_zone)
+	if err != nil {
+		return nil, err
+	}
+	return resolved_ll_addr, nil
+}
+
+func FindNeighbors(dev net.Interface, ll_addr *net.IPAddr, key node.PublicKey) <-chan node.NodeAddress {
 	conn, err := l2.ConnectExistingDevice(dev.Name)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	nf := node.NewNeighborData(key)
+	nf := node.NewNeighborData(key, ll_addr)
 	channel, err := nf.Find(dev.HardwareAddr, conn)
 	if err != nil {
 		log.Fatal(err)
@@ -37,7 +57,12 @@ func main() {
 
 	// find neighbors of each interface
 	for _, dev := range devs {
-		neighbours := FindNeighbors(dev, public_key)
+		ll_addr, err := GetLinkLocalAddr(dev)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		neighbours := FindNeighbors(dev, ll_addr, public_key)
 		for addr := range neighbours {
 			log.Printf("Neighbour Found %v", addr)
 			connection, err := node.EstablishSSH("Dummy address", key)
