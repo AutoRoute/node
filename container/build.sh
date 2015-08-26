a25621ee11a71ae845f5f2d5b691f542e7af21c7
Colin L. Rice <colin@daedrum.net>
2015-08-26 03:56:35 -0400

    Hardcode a bunch of the basic files

--- container/build.sh
+++ container/build.sh
@@ -12,8 +12,8 @@ cp /usr/lib/libnss_dns.so.2 fs/usr/lib
 mkdir fs/lib64
 cp /lib64/ld-linux-x86-64.so.2 fs/lib64
 mkdir fs/etc
-cp /etc/host.conf fs/etc
-cp /etc/nsswitch.conf fs/etc
-cp /etc/hosts fs/etc
-cp /etc/resolv.conf fs/etc
+touch fs/etc/hosts
+echo 'nameserver 8.8.4.4' > fs/etc/resolv.conv
+echo 'nameserver 8.8.8.8' >> fs/etc/resolv.conv
+echo 'hosts: files dns' > fs/etc/nsswitch.conf
 docker build .
