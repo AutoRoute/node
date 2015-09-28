ef091ddb75364aa14cbcc9ebf13c33976c03ceaa
derekameer <meerd@rpi.edu>
2015-09-28 05:19:16 -0400

    Added support for building docker containers on multiple distros, not just Arch. find_distro.sh returns a string corresponding to your linux distro.

---
+++ container/find_distro.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Shell script which detects the Linux distro it's running on
+
+detectedDistro="Unknown"
+regExpLsbInfo="Description:[[:space:]]*([^ ]*)"
+regExpLsbFile="/etc/(.*)[-_]"
+
+if [ `which lsb_release 2>/dev/null` ]; then       # lsb_release available
+   lsbInfo=`lsb_release -d`
+   if [[ $lsbInfo =~ $regExpLsbInfo ]]; then
+      detectedDistro=${BASH_REMATCH[1]}
+   else
+      echo "??? Should not occur: Don't find distro name in lsb_release output ???"
+      exit 1
+   fi
+
+else                                               # lsb_release not available
+   etcFiles=`ls /etc/*[-_]{release,version} 2>/dev/null`
+   for file in $etcFiles; do
+      if [[ $file =~ $regExpLsbFile ]]; then
+         detectedDistro=${BASH_REMATCH[1]}
+         break
+      else
+         echo "??? Should not occur: Don't find any etcFiles ???"
+         exit 1
+      fi
+   done
+fi
+
+detectedDistro=`echo $detectedDistro | tr "[:upper:]" "[:lower:]"`
+
+case $detectedDistro in
+	suse) 	detectedDistro="opensuse" ;;
+        linux)	detectedDistro="linuxmint" ;;
+esac
+
+echo $detectedDistro
