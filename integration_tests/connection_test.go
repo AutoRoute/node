efc673f1905a5839fe42049abbac267741c914e8
Colin L. Rice <colin@daedrum.net>
2015-11-10 10:59:49 -0500

    Turn the integration test into having more helpers

--- integration_tests/connection_test.go
+++ integration_tests/connection_test.go
@@ -1,25 +1,57 @@
 package integration_tests
 
 import (
+	"errors"
 	"testing"
 	"time"
 )
+
+func WaitForID(b Binary) (string, error) {
+	timeout := time.After(1 * time.Second)
+	for range time.Tick(10 * time.Millisecond) {
+		select {
+		case <-timeout:
+			return "", errors.New("Timeout while waiting for id")
+		default:
+		}
+		id, err := b.GetID()
+		if err != nil {
+			continue
+		}
+		return id, nil
+	}
+	panic("unreachable")
+}
+
+func WaitForConnection(b Binary, addr string) error {
+	stop := time.After(10 * time.Second)
+	for range time.Tick(10 * time.Millisecond) {
+		select {
+		case <-stop:
+			return errors.New("Timeout out while waiting for connection")
+		default:
+		}
+
+		connections, err := b.GetConnections()
+		if err != nil {
+			continue
+		}
+		for _, address := range connections {
+			if address == addr {
+				return nil
+			}
+		}
+	}
+	panic("unreachable")
+}
 
 func TestConnection(t *testing.T) {
 	listen := NewNodeBinary(BinaryOptions{listen: "localhost:9999", fake_money: true})
 	listen.Start()
 	defer listen.KillAndPrint(t)
-	timeout := time.After(1 * time.Second)
-	for range time.Tick(10 * time.Millisecond) {
-		select {
-		case <-timeout:
-			t.Fatalf("listen binary failed to come up")
-		default:
-		}
-		_, err := listen.GetID()
-		if err == nil {
-			break
-		}
+	_, err := WaitForID(listen)
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	connect := NewNodeBinary(BinaryOptions{
@@ -28,29 +60,10 @@ func TestConnection(t *testing.T) {
 		fake_money: true})
 	connect.Start()
 	defer connect.KillAndPrint(t)
+	connect_id, err := WaitForID(connect)
 
-	stop := time.After(10 * time.Second)
-	for range time.Tick(10 * time.Millisecond) {
-		select {
-		case <-stop:
-			t.Fatalf("Failed to get find connection")
-		default:
-		}
-		connect_id, err := connect.GetID()
-		if err != nil {
-			continue
-		}
-
-		listen_connections, err := listen.GetConnections()
-		if err != nil {
-			continue
-		}
-		if len(listen_connections) == 0 {
-			continue
-		}
-		if listen_connections[0] != connect_id {
-			t.Fatalf("Expected %x, got %x", connect_id, listen_connections[0])
-		}
-		break
+	err = WaitForConnection(listen, connect_id)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
