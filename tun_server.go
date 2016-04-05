3c39e5e8d24e9f10d5da55cd94481a57f536e4e1
Eric Oswald <eoswald39@gmail.com>
2016-04-05 17:20:00 -0400

    Started dispatcher, keep track of ip's

--- tun_server.go
+++ tun_server.go
@@ -9,11 +9,16 @@ import (
 	"github.com/AutoRoute/node/types"
 )
 
+var request_header string = "hello"
+var begin_ip_block net.IP = net.ParseIP("6666::0")
+var end_ip_block net.IP = net.PasrseIP("6666::6666")
+
 type TunServer struct {
   data        DataConnection
 	dev         tuntap.Interface
 	nodes       map[net.IP]types.NodeAddress
 	connections map[types.NodeAddress]*TCP
+  curr_ip     int
 }
 
 func NewTunServer(d DataConnection) *TunServer {
@@ -21,7 +26,7 @@ func NewTunServer(d DataConnection) *TunServer {
 	if err != nil {
 		log.Fatal(err)
 	}
-	return &TunServer{d, i, make(map[net.TIP]types.NodeAddress), make(map[net.IP]*TCP)}
+	return &TunServer{d, i, make(map[net.TIP]types.NodeAddress), make(map[net.IP]*TCP, 0)}
 }
 
 func (ts *TunServer) Connect(tcptun string, amt int64) {
@@ -31,13 +36,19 @@ func (ts *TunServer) Connect(tcptun string, amt int64) {
     log.Fatal(err)
   }
   t := NewTCPTunnel(ts.dev, ts.data, types.NodeAddress(dest), amt)
+  ip := net.ParseIP(net.Sprintf("6666::%d", ts.curr_ip++))
+  nodes[ip] = types.NodeAddress(dest)
   connections[types.NodeAddress] = t
 }
 
 func (ts *TunServer) Listen() {
   go func() {
     for packet := range data.Packets() {
-      Connect(packet.Data, 10000)
+      if packet.Data == request_header {
+        Connect(packet.Data, 10000)
+      } else {
+        /* do something with packet */
+      }
     }
   }()
 }
