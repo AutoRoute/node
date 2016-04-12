7eec67e1c5fdda74141c16267e120d2607692e80
Daniel Petti <djpetti@gmail.com>
2016-04-11 21:32:08 -0400

    Make bandwidth routing use routingDecision channel.
    This method of communicating is more standard than what was in
    use before. I also rethought some of the dependency injection
    stuff to make initialization code cleaner.

--- internal/bandwidth.go
+++ internal/bandwidth.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"log"
+	"sync"
 	"time"
 
 	"github.com/AutoRoute/node/types"
@@ -9,49 +10,112 @@ import (
 
 // A bandwidth estimator estimates bandwidth between us and a neighbor and
 // takes care of making intelligent routing decisions.
-// NOTE: bandwidthEstimator does not inherently support concurrency. Calls to
-// its methods should be protected.
+// NOTE: bandwidthEstimator is meant to be used as a service. It will monitor
+// bandwidth in the background.
 type bandwidthEstimator struct {
+	// routingDecision channel that our data is coming from.
+	outgoing <-chan routingDecision
+
 	// Current estimated bandwidths.
-	bandwidth map[types.NodeAddress]float64
+	bandwidth      map[types.NodeAddress]float64
+	bandwidth_lock sync.RWMutex
+
 	// Total number of bytes sent.
 	bytes_sent map[types.NodeAddress]int64
 	// Total elapsed time waiting for that data to send.
 	sent_time map[types.NodeAddress]time.Duration
 	// Time at which the last packet send started.
 	send_start_time map[types.NodeAddress]time.Time
+	// This is so we can keep track of packets that came from the same source. It
+	// maps each source to the next hop of the last packet sent from that source.
+	last_sent_to map[types.NodeAddress]types.NodeAddress
+
+	// Channel to indicate when it's time to quit.
+	quit chan bool
 }
 
 // Creates a new bandwidthEstimator.
-func NewBandwidthEstimator() *bandwidthEstimator {
+// Args:
+//  routing: The routingHan.
+// Returns:
+//  A new bandwidthEstimator.
+func newBandwidthEstimator(routing <-chan routingDecision) *bandwidthEstimator {
 	estimator := bandwidthEstimator{
+		routing,
 		make(map[types.NodeAddress]float64),
+		sync.RWMutex{},
 		make(map[types.NodeAddress]int64),
 		make(map[types.NodeAddress]time.Duration),
 		make(map[types.NodeAddress]time.Time),
+		make(map[types.NodeAddress]types.NodeAddress),
+		make(chan bool),
 	}
+
+	// Start the background service.
+	go estimator.monitorPackets()
+
 	return &estimator
 }
 
-// This should be called every time we're about to send a packet to someone.
-// Args:
-//  node: Where we're going to send the packet.
-func (b *bandwidthEstimator) WillSendPacket(node types.NodeAddress) {
-	b.send_start_time[node] = time.Now()
+// Monitors packets that get sent through our routingHandler, and uses that data
+// to glean bandwidth information.
+func (b *bandwidthEstimator) monitorPackets() {
+	// Channel that we use for monitoring.
+	for {
+		select {
+		case packet, ok := <-b.outgoing:
+			if !ok {
+				log.Print("bandwidthEstimator: Routes channel closed, exiting.")
+				return
+			}
+			b.sentPacket(packet.source, packet.nexthop, packet.size)
+		case <-b.quit:
+			return
+		}
+	}
 }
 
 // This should be called after a packet is sent.
 // Args:
+//  src: Where the packet is coming from.
 //  node: Where we sent the packet.
 //  size: How many bytes the packet is.
-func (b *bandwidthEstimator) SentPacket(node types.NodeAddress, size int64) {
+func (b *bandwidthEstimator) sentPacket(src types.NodeAddress,
+	node types.NodeAddress, size int) {
+	use_node := node
+
+	last_hop, ok := b.last_sent_to[src]
+	if last_hop != node {
+		// If we're sending it from the same source to a different node, since each
+		// source is handled by one goroutine, that means the previous send
+		// completed.
+		use_node = last_hop
+	}
+
+	b.last_sent_to[src] = node
+	if !ok {
+		// If we have no previous data, there's not much we can do in the way of
+		// bandwidth.
+		b.send_start_time[node] = time.Now()
+		return
+	}
 
 	// Calculate new average.
-	b.bytes_sent[node] += size
-	elapsed := time.Since(b.send_start_time[node])
-	b.sent_time[node] += elapsed
-	b.bandwidth[node] = float64(b.bytes_sent[node]) /
-		(float64(b.sent_time[node]) / float64(time.Second))
+	b.bytes_sent[use_node] += int64(size)
+
+	start_time := b.send_start_time[use_node]
+	elapsed := time.Since(start_time)
+	b.sent_time[use_node] += elapsed
+
+	b.bandwidth_lock.Lock()
+	b.bandwidth[use_node] = float64(b.bytes_sent[use_node]) /
+		(float64(b.sent_time[use_node]) / float64(time.Second))
+	b.bandwidth_lock.Unlock()
+
+	// Mark the time now. Assuming the connection is fairly saturated, this should
+	// be accurate for calculating how long it took to send the next packet. If
+	// it's not saturated, well, then this calculation doesn't really matter. :)
+	b.send_start_time[node] = time.Now()
 }
 
 // Calculates and returns weights for each node in order to make routing
@@ -63,6 +127,9 @@ func (b *bandwidthEstimator) SentPacket(node types.NodeAddress, size int64) {
 func (b *bandwidthEstimator) GetWeights(nodes []types.NodeAddress) []float64 {
 	// First, we need to calculate the total bandwidth so we know how to scale it.
 	total := float64(0)
+	b.bandwidth_lock.RLock()
+  defer b.bandwidth_lock.RUnlock()
+
 	for _, node := range nodes {
 		total += b.bandwidth[node]
 	}
@@ -84,8 +151,10 @@ func (b *bandwidthEstimator) GetWeights(nodes []types.NodeAddress) []float64 {
 		// We don't have a valid bandwidth yet.
 		weight := standard_weight
 		num_unknown += 1
+
 		log.Printf("bandwidth: %f, weight: %f\n",
 			b.bandwidth[node], weight)
+
 		weights = append(weights, weight)
 	}
 
@@ -106,4 +175,9 @@ func (b *bandwidthEstimator) GetWeights(nodes []types.NodeAddress) []float64 {
 	}
 
 	return weights
+}
+
+// Stop the bandwidth estimator.
+func (b *bandwidthEstimator) Close() {
+	close(b.quit)
 }
