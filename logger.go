4cc7b2224aac3aa9004694ba238cdc46084f8209
Eric Oswald <eoswald39@gmail.com>
2016-10-04 17:31:17 -0400

    Lock json encoder when using.

--- logger.go
+++ logger.go
@@ -3,6 +3,7 @@ package node
 import (
 	"encoding/json"
 	"io"
+	"sync"
 
 	"github.com/AutoRoute/node/internal"
 	"github.com/AutoRoute/node/types"
@@ -17,16 +18,21 @@ type routingDecision struct {
 
 type Logger struct {
 	log_enc *json.Encoder
+	lock    *sync.Mutex
 }
 
 func NewLogger(w io.Writer) Logger {
-	return Logger{json.NewEncoder(w)}
+	return Logger{json.NewEncoder(w), new(sync.Mutex)}
 }
 
 func (lgr Logger) LogBloomFilter(brm *internal.BloomReachabilityMap) error {
+	lgr.lock.Lock()
+	defer lgr.lock.Unlock()
 	return lgr.log_enc.Encode(brm.Conglomerate)
 }
 
 func (lgr Logger) LogRoutingDecision(dest types.NodeAddress, next types.NodeAddress, packet_size int, amt int64) error {
+	lgr.lock.Lock()
+	defer lgr.lock.Unlock()
 	return lgr.log_enc.Encode(routingDecision{dest, next, packet_size, amt})
 }
